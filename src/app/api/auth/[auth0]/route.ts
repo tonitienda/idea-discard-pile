c54240258add36916e06c04b5386ab66f426fb3a
Toni Tienda <tonitienda@gmail.com>
2024-08-22 18:01:31 +0200

    chore: updates login with auth0 to redirect to the vercel url

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,17 +1,4 @@
 // app/api/auth/[auth0]/route.js
 import { handleAuth } from "@auth0/nextjs-auth0";
 
-const getAuth0BaseUrl = () => {
-  if (
-    process.env.VERCEL_ENV === "preview" ||
-    process.env.VERCEL_ENV === "production"
-  ) {
-    return `https://${process.env.VERCEL_URL}`;
-  }
-
-  return process.env.AUTH0_BASE_URL || "http://localhost:3000";
-};
-
-process.env.AUTH0_BASE_URL = getAuth0BaseUrl();
-
 export const GET = handleAuth();
