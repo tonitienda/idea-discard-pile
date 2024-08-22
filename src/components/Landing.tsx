80f04a26332cad4a37643f3027ffe0f78ed69a05
Toni Tienda <tonitienda@gmail.com>
2024-08-23 00:04:16 +0200

    feature: landing page design update

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -16,57 +16,68 @@ const ExampleIdeas = [
 
 export default function Landing() {
   return (
-    <div className="container-fluid">
-      <div className="row h-100">
-        <div className="col-md-7 d-flex align-items-center justify-content-center bg-light">
-          <div className="row">
-            <h1>IDEA DISCARD PILE</h1>
-            <h2>Where Ideas Find Their Second Chance</h2>
-            <h5>
-              Whether you’re letting go of a thought or searching for your next
-              spark of inspiration,
-              <b className="text-success">Idea Discard Pile</b> is the place
-              where ideas are shared, revived, and reimagined.
-            </h5>
-            <p>
-              Got an idea that didn’t quite take off? Don’t let it gather
-              dust—share it here and see where it might lead. Whether it’s a
-              wild brainstorm, an unfinished concept, or a side project you
-              couldn’t complete, your ideas deserve a second look.{" "}
-            </p>
-            <p>
-              Or maybe you’re looking for inspiration? Explore a treasure trove
-              of discarded thoughts and half-baked plans from other creatives.
-            </p>
-            <p>
-              Whether you need a fresh idea to kickstart a new project or just
-              want to see what others are thinking, you’ll find plenty of
-              inspiration here. Join today to share, explore, and breathe new
-              life into ideas that deserve another chance.
-            </p>
-            <div className="row" style={{ paddingTop: 48 }}>
-              <div className="example-ideas mb-5">
-                {ExampleIdeas.map((idea, index) => (
-                  <div key={index} style={{ animationDelay: `${4 * index}s` }}>
-                    <h2>{idea}</h2>
-                  </div>
-                ))}
+    <div className="row h-100">
+      <div className="col-md-8 align-items-left justify-content-left bg-light">
+        <div
+          className="row"
+          style={{ padding: 24, textAlign: "justify", height: "auto" }}
+        >
+          <h1>IDEA DISCARD PILE</h1>
+          <h2>Where Ideas Find Their Second Chance</h2>
+          <h5 style={{ marginBottom: 24 }}>
+            Whether you’re letting go of a thought or searching for your next
+            spark of inspiration,
+            <b className="text-success">Idea Discard Pile</b> is the place where
+            ideas are shared, revived, and reimagined.
+          </h5>
+          <p>
+            Got an idea that didn’t quite take off? Don’t let it gather
+            dust—share it here and see where it might lead. Whether it’s a wild
+            brainstorm, an unfinished concept, or a side project you couldn’t
+            complete, your ideas deserve a second look.{" "}
+          </p>
+          <p>
+            Or maybe you’re looking for inspiration? Explore a treasure trove of
+            discarded thoughts and half-baked plans from other creatives.
+          </p>
+          <p>
+            Whether you need a fresh idea to kickstart a new project or just
+            want to see what others are thinking, you’ll find plenty of
+            inspiration here. Join today to share, explore, and breathe new life
+            into ideas that deserve another chance.
+          </p>
+        </div>
+        <div className="row">
+          <div className="example-ideas">
+            {ExampleIdeas.map((idea, index) => (
+              <div key={index} style={{ animationDelay: `${4 * index}s` }}>
+                <h2>{idea}</h2>
               </div>
-            </div>
+            ))}
           </div>
         </div>
+      </div>
 
-        <div className="col-md-5 d-flex align-items-center justify-content-center bg-white">
-          <div className="d-grid gap-2">
-            <a href="/api/auth/login">
-              <button
-                className="btn btn-lg btn-success"
-                style={{ height: 100, width: 200 }}
-              >
-                Join the Idea Pile
-              </button>
-            </a>
-          </div>
+      <div
+        className="col-md-4 d-flex align-items-center justify-content-center bg-white"
+        style={{
+          padding: 24,
+          backgroundImage: "url(/images/hero.webp) ",
+          backgroundSize: "contain",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: "auto",
+        }}
+      >
+        <div className="d-grid gap-2 ">
+          <a href="/api/auth/login">
+            <button
+              className="btn btn-lg btn-success"
+              style={{ height: 100, width: 200 }}
+            >
+              Join the Idea Pile
+            </button>
+          </a>
         </div>
       </div>
     </div>
