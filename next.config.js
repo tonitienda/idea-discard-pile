826d60ec8959d7eeeb380d605a0367cd83d14ac1
Toni Tienda <tonitienda@gmail.com>
2024-08-22 22:07:41 +0200

    Feature/make base auth0 dynamic (#4)
    * chore: updates login with auth0 to redirect to the current url

---
+++ next.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+
+const nextConfig = {
+  env: {
+    AUTH0_BASE_URL: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : process.env.AUTH0_BASE_URL,
+  },
+};
+
+module.export = nextConfig;
