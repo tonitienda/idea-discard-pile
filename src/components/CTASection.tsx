20e31f43f2749796c0602fe768297ea519f534a6
Toni Tienda <tonitienda@gmail.com>
2024-08-23 12:45:58 +0200

    chore: adds login link again to the cta in landing page

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -3,7 +3,16 @@ export default function CTASection() {
   return (
     <section className="text-center my-5">
       <h2 className="mb-4">Ready to Turn Ideas into Reality?</h2>
-      <button className="btn btn-primary btn-lg">Share Your Idea</button>
+      {
+        // TODO - Add a list of texts for the button:
+        // Share your idea
+        // Get inspired
+        // Start collaborating
+        // And iterate over the list to render a button for each text
+      }
+      <a href="/api/auth/login">
+        <button className="btn btn-primary btn-lg">Share Your Idea</button>
+      </a>
     </section>
   );
 }
