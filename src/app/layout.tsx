dee0edb4731dd560e8429beb3b503d1f80bff4c2
Toni Tienda <tonitienda@gmail.com>
2024-08-24 06:21:15 +0200

    feat: stores ideas in memory and tracks its owner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,9 +46,6 @@ export default async function RootLayout({
       </head>
       <UserProvider>
         <body>
-          {
-            // Template: {template}
-          }
           {user && <NavBar />}
           {user ? (
             <div className="container" style={{ paddingTop: 24 }}>
