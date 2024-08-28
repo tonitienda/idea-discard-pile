f688a3be2db20c8d7264c6b0179d8715e925adb7
Toni Tienda <tonitienda@gmail.com>
2024-08-28 17:26:48 +0200

    chore: adds link to the feedback in typeforms

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,10 +4,19 @@ export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          A Pile of Ideas
-        </a>
+        <div>
+          <a className="navbar-brand" href="/">
+            A Pile of Ideas
+          </a>
+          <a
+            href="https://y5mtc76sp1w.typeform.com/to/gqJGirOo"
+            target="_blank"
+          >
+            We ❤️ Feedback
+          </a>
+        </div>
         <a href="/ideas">My ideas</a>
+
         <User />
       </div>
     </nav>
