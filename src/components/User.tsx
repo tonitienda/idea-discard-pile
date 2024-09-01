0d07bbed75d00cee5cce136ae9e853363e674009
Toni Tienda <tonitienda@gmail.com>
2024-09-01 04:03:25 +0200

    fix: fixes navbar to show the user data from db and not from auth0

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,16 +1,16 @@
 import { getSession } from "@auth0/nextjs-auth0";
+import { useUser } from "../app/hooks/use-user";
 
 export default async function User() {
-  const session = await getSession();
-  const user = session ? session.user : null;
+  const user = await useUser();
   return user ? (
     <div>
       <img
         src={user.picture}
-        alt={user.name}
+        alt={user.name || user.handle}
         style={{ borderRadius: "50%", width: 25, height: 25 }}
       />
-      <span>{user.name}</span>
+      <span>{user.name || `@${user.handle}`}</span>
       <a href="/api/auth/logout" style={{ marginLeft: 12 }}>
         <span>Logout</span>
       </a>
